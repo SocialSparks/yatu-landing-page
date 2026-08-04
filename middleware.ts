0e46a735d9dd0673864cdcc352cc305c97449c6b
Celian Frasca <celian.frasca@gmail.com>
2026-08-04 15:45:59 +0200

    fix: redirect from Next.js

--- middleware.ts
+++ middleware.ts
@@ -56,5 +56,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next|favicon.ico|_not-found|_error).*)"],
 };
